540e76688c5451d114ea71bac9fb2556d76406c3
rezerbit <rezerbit@gmail.com>
2023-01-10 23:03:47 +0500

    Add kitchen-calculator iteration 3

--- elixir/kitchen-calculator/lib/kitchen_calculator.ex
+++ elixir/kitchen-calculator/lib/kitchen_calculator.ex
@@ -1,5 +1,4 @@
 defmodule KitchenCalculator do
-  @units [:milliliter, :cup, :fluid_ounce, :teaspoon, :tablespoon]
   @ml %{
     milliliter: 1,
     cup: 240,
@@ -8,21 +7,8 @@ defmodule KitchenCalculator do
     tablespoon: 15
   }
 
-  def get_volume({unit, volume}) when unit in @units do
-    volume
-  end
-
-  def to_milliliter({unit, volume}) when unit in @units do
-    {:milliliter, @ml[unit] * volume}
-  end
-
-  def from_milliliter({:milliliter, volume}, unit) when unit in @units do
-    {unit, volume / @ml[unit]}
-  end
-
-  def convert(volume_pair, unit) when unit in @units do
-    volume_pair
-    |> to_milliliter()
-    |> from_milliliter(unit)
-  end
+  def get_volume({_unit, volume}), do: volume
+  def to_milliliter({unit, volume}), do: {:milliliter, @ml[unit] * volume}
+  def from_milliliter({:milliliter, volume}, unit), do: {unit, volume / @ml[unit]}
+  def convert(volume_pair, unit), do: volume_pair |> to_milliliter() |> from_milliliter(unit)
 end
