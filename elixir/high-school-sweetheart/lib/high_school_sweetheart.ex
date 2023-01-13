79149c000db7c914b2e884498aa001bf1d5a8aa1
rezerbit <rezerbit@gmail.com>
2023-01-13 20:14:27 +0500

    Add high-school-sweetheart iteration 2

--- elixir/high-school-sweetheart/lib/high_school_sweetheart.ex
+++ elixir/high-school-sweetheart/lib/high_school_sweetheart.ex
@@ -1,10 +1,15 @@
 defmodule HighSchoolSweetheart do
   def first_letter(name) do
-    name |> String.trim() |> String.first()
+    name
+    |> String.trim()
+    |> String.first()
   end
 
   def initial(name) do
-    (name |> first_letter() |> String.upcase()) <> "."
+    name
+    |> first_letter()
+    |> String.upcase()
+    |> Kernel.<>(".")
   end
 
   def initials(full_name) do
@@ -15,13 +20,16 @@ defmodule HighSchoolSweetheart do
   end
 
   def pair(full_name1, full_name2) do
+    i1 = initials(full_name1)
+    i2 = initials(full_name2)
+
     """
          ******       ******
        **      **   **      **
      **         ** **         **
     **            *            **
     **                         **
-    **     #{initials(full_name1)}  +  #{initials(full_name2)}     **
+    **     #{i1}  +  #{i2}     **
      **                       **
        **                   **
          **               **
